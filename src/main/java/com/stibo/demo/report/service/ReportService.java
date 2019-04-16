bfb656f6c7b7e822ba61970961c91479a59f0ec3
pawelkaczmarekuk <pawel.kaczmarek@gmail.com>
2019-04-16 20:51:43 +0200

    solution

--- src/main/java/com/stibo/demo/report/service/ReportService.java
+++ src/main/java/com/stibo/demo/report/service/ReportService.java
@@ -2,13 +2,66 @@ package com.stibo.demo.report.service;
 
 import com.stibo.demo.report.model.*;
 import org.springframework.stereotype.Service;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @Service
 public class ReportService {
-    public Stream<Stream<String>> report(Datastandard datastandard, String categoryId) {
-        throw new NotImplementedException();
+
+    public Stream<Stream<String>> report(final Datastandard datastandard, final String categoryId) {
+
+        if (datastandard == null) {
+            throw new NullPointerException("datastandard must not be null");
+        }
+
+        if (categoryId == null) {
+            throw new NullPointerException("categoryId must not be null");
+        }
+
+        return datastandard
+                .getCategories()
+                .stream()
+                .filter(category -> category.getId().equals(categoryId))
+                .flatMap( category ->
+                    category.getAttributeLinks().stream().map( attributeLink ->
+                        findAttribute(datastandard, attributeLink)
+                                .map( attribute -> {
+                                    StringBuilder nestedTypes = new StringBuilder();
+                                    resolveNestedTypes(datastandard, attributeLink, nestedTypes);
+                                    return Stream.of(
+                                            category.getName(),
+                                            attributeLink.getOptional() ? String.format("%s*", attribute.getName()) : attribute.getName(),
+                                            attribute.getDescription() == null ? "" : attribute.getDescription(),
+                                            attribute.getType().getMultiValue() ? String.format("%s{\n%s\n}[]", attribute.getType().getId(), nestedTypes) : attribute.getType().getId(),
+                                            datastandard.getAttributeGroups().stream().filter(attributeGroup -> attribute.getGroupIds().indexOf(attributeGroup.getId()) >= 0)
+                                                    .map(AttributeGroup::getName).sorted().collect(Collectors.joining("\n"))
+                                    );
+                                }).orElseGet( null)
+
+                    ).filter(Objects::nonNull));
+
     }
+
+    public void resolveNestedTypes(final Datastandard datastandard, final AttributeLink theAttributeLink, StringBuilder stringBuilder) {
+        stringBuilder.append("  ");
+        findAttribute(datastandard, theAttributeLink).ifPresent(theAttribute -> {
+            stringBuilder.append(String.format("%s%s: %s", theAttribute.getName(), theAttributeLink.getOptional() ? "*" : "", theAttribute.getType().getId()));
+            if (theAttribute.getAttributeLinks() != null) {
+                theAttribute.getAttributeLinks().forEach(attributeLink -> {
+                    stringBuilder.append("\n");
+                    resolveNestedTypes(datastandard, attributeLink, stringBuilder);
+                });
+            }
+        });
+
+    }
+
+    public Optional<Attribute> findAttribute(final Datastandard datastandard, final AttributeLink attributeLink) {
+        return datastandard.getAttributes().stream().filter(attribute -> attribute.getId().equals(attributeLink.getId())).findFirst();
+    }
+
 }
+
--- src/test/java/com/stibo/demo/report/service/ReportServiceTest.java
+++ src/test/java/com/stibo/demo/report/service/ReportServiceTest.java
@@ -2,6 +2,8 @@ package com.stibo.demo.report.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.stibo.demo.report.model.Datastandard;
+import org.apache.logging.log4j.util.Strings;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -34,7 +36,39 @@ public class ReportServiceTest {
     }
 
     @Test
-    public void testReport() {
+    public void testReportLeaf() {
         List<List<String>> report = reportService.report(datastandard, "leaf").map(row -> row.collect(toList())).collect(toList());
+
+        Assert.assertEquals("Leaf", report.get(0).get(0));
+        Assert.assertEquals("Composite Value*", report.get(0).get(1));
+        Assert.assertEquals("Composite Value Description", report.get(0).get(2));
+        Assert.assertEquals("composite{\n  Composite Value*: composite\n  Nested Value: integer\n}[]", report.get(0).get(3));
+        Assert.assertEquals("All\nComplex", report.get(0).get(4));
+    }
+
+    @Test
+    public void testReportRoot() {
+        List<List<String>> report = reportService.report(datastandard, "root").map(row -> row.collect(toList())).collect(toList());
+        Assert.assertEquals("Root", report.get(0).get(0));
+        Assert.assertEquals("String Value", report.get(0).get(1));
+        Assert.assertEquals("", report.get(0).get(2));
+        Assert.assertEquals("string", report.get(0).get(3));
+        Assert.assertEquals("All", report.get(0).get(4));
+    }
+
+    @Test
+    public void testNonExistingCategory() {
+        List<List<String>> report = reportService.report(datastandard, "doesnotexist").map(row -> row.collect(toList())).collect(toList());
+        Assert.assertTrue(report.isEmpty());
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testDataStandardNull() {
+        reportService.report(null, "root");
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testCategoryIdNull() {
+        reportService.report(datastandard, null);
     }
 }
